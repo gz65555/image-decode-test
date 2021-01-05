da4534a5b35e2dcc9b18d635aa31c59d86c8b996
胡松 <kevin.hs@antfin.com>
2021-01-05 21:50:18 +0800

    feat: img generate

--- process.js
+++ process.js
@@ -1,2 +1,18 @@
 const sharp = require("sharp");
 
+let index = 0;
+
+const basic = sharp("template_img/1.jpg");
+basic.constructor.prototype.toImagesFile = function () {
+  index++;
+  this.toFile(`imgs/${index}.jpg`);
+};
+
+basic.toImagesFile();
+
+for (let i = 0; i < 500; i++) {
+  basic.blur(i + 1);
+  const a = Math.ceil(Math.random() * 10) / 10;
+  const b = Math.ceil(Math.random() * 10) / 10;
+  basic.linear(a, b).toImagesFile();
+}
