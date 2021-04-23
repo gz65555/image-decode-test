eee2e7ed440fb3468214cecb786c5078693df8ca
胡松 <kevin.hs@antfin.com>
2021-04-23 11:29:19 +0800

    doc: update readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# 测试方法
+
+1. 生成图片
+
+``` shell
+npm run generate
+```
+
+2. 启动服务器
+
+``` shell
+npm run serve
+```
+
--- process.js
+++ process.js
@@ -1,18 +1,25 @@
 const sharp = require("sharp");
+const fs = require("fs");
+
+const directoryExisting = fs.existsSync("./imgs");
+
+if (!directoryExisting) {
+	fs.mkdirSync("./imgs");
+}
 
 let index = 0;
 
 const basic = sharp("template_img/1.jpg");
 basic.constructor.prototype.toImagesFile = function () {
-  index++;
-  this.toFile(`imgs/${index}.jpg`);
+	index++;
+	this.toFile(`imgs/${index}.jpg`);
 };
 
 basic.toImagesFile();
 
-for (let i = 0; i < 500; i++) {
-  basic.blur(i + 1);
-  const a = Math.ceil(Math.random() * 10) / 10;
-  const b = Math.ceil(Math.random() * 10) / 10;
-  basic.linear(a, b).toImagesFile();
+for (let i = 0; i < 100; i++) {
+	basic.blur(i + 1);
+	const a = Math.ceil(Math.random() * 10) / 10;
+	const b = Math.ceil(Math.random() * 10) / 10;
+	basic.linear(a, b).toImagesFile();
 }
